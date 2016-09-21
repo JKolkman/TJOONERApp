2c1b2a3dfaf3eb8bdc9446baea8950c4d4b6983f
Joost Kolkman <joost_kolkman@hotmail.nl>
2016-09-21 15:09:47 +0200

    Initial commit

---
+++ app/src/main/java/ehi2vsa/tjoonerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package ehi2vsa.tjoonerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
