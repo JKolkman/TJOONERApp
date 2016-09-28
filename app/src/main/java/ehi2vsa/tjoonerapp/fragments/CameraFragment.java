c6803bf79536e23549cfbe36f4669ddf0833d7ae
stan <stansmeitink@hotmail.com>
2016-09-28 17:46:22 +0200

    CameraFragment upgedate: start camera op

--- app/src/main/java/ehi2vsa/tjoonerapp/fragments/CameraFragment.java
+++ app/src/main/java/ehi2vsa/tjoonerapp/fragments/CameraFragment.java
@@ -1,24 +1,28 @@
 package ehi2vsa.tjoonerapp.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ehi2vsa.tjoonerapp.R;
 
-/**
- * Created by joost on 28/09/2016.
- */
 public class CameraFragment extends Fragment {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
 
         return view;
     }
