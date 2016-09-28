1852cb4673be150e88be1b7555f45e752b8f3418
djtjp <thijsperk@gmail.com>
2016-09-28 17:45:12 +0200

    Now we can view images

--- app/src/main/java/ehi2vsa/tjoonerapp/fragments/GalleryFragment.java
+++ app/src/main/java/ehi2vsa/tjoonerapp/fragments/GalleryFragment.java
@@ -6,8 +6,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import ehi2vsa.tjoonerapp.R;
+import ehi2vsa.tjoonerapp.adapters.CustomImageAdapter;
 
 /**
  * Created by joost on 28/09/2016.
@@ -18,7 +21,20 @@ public class GalleryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
 
+        GridView gridview = (GridView) view.findViewById(R.id.gridview);
+        gridview.setAdapter(new CustomImageAdapter(this.getActivity()));
 
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
 }
