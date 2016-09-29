57391d90d1d4f76044b60d1732d8623591c8cb50
stan <stansmeitink@hotmail.com>
2016-09-29 16:42:44 +0200

    CameraFragment upgedate: file met de foto erin gecreëerd

--- app/src/main/java/ehi2vsa/tjoonerapp/fragments/CameraFragment.java
+++ app/src/main/java/ehi2vsa/tjoonerapp/fragments/CameraFragment.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -13,11 +14,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import ehi2vsa.tjoonerapp.R;
 
 public class CameraFragment extends Fragment {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView ivResult;
+    private String mCurrentPhotoPath;
 
     @Nullable
     @Override
@@ -26,6 +33,7 @@ public class CameraFragment extends Fragment {
 
         ivResult = (ImageView) view.findViewById(R.id.ivResult);
         Button btnCamera = (Button) view.findViewById(R.id.btnCamera);
+        Button btnSave = (Button) view.findViewById(R.id.btnSave);
 
         btnCamera.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -34,7 +42,12 @@ public class CameraFragment extends Fragment {
             }
         });
 
-        dispatchTakePictureIntent();
+        btnSave.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
@@ -43,11 +56,10 @@ public class CameraFragment extends Fragment {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } else {
-            getFragmentManager().beginTransaction().replace(R.id.fragment_container, new MediaFragment()).commit();
         }
     }
 
+    // TODO: This sht!
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
@@ -55,5 +67,21 @@ public class CameraFragment extends Fragment {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             ivResult.setImageBitmap(imageBitmap);
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
