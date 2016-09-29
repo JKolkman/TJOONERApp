a234a3b5c046bbf242c33209a980fd3fb831fb83
stan <stansmeitink@hotmail.com>
2016-09-29 11:09:55 +0200

    CameraFragment upgedate: start camera op en returned thumbnail

--- app/src/main/java/ehi2vsa/tjoonerapp/fragments/CameraFragment.java
+++ app/src/main/java/ehi2vsa/tjoonerapp/fragments/CameraFragment.java
@@ -1,29 +1,59 @@
 package ehi2vsa.tjoonerapp.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import ehi2vsa.tjoonerapp.R;
 
 public class CameraFragment extends Fragment {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView ivResult;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
+        ivResult = (ImageView) view.findViewById(R.id.ivResult);
+        Button btnCamera = (Button) view.findViewById(R.id.btnCamera);
+
+        btnCamera.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        dispatchTakePictureIntent();
+
+        return view;
+    }
+
+    public void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } else {
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container, new MediaFragment()).commit();
         }
+    }
 
-        return view;
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ivResult.setImageBitmap(imageBitmap);
+        }
     }
 }
