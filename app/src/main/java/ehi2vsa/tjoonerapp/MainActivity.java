41162e110a488e925c129e4f6676495031e536a4
Joost Kolkman <joost_kolkman@hotmail.nl>
2016-10-12 14:35:20 +0200

    removed inner class

--- app/src/main/java/ehi2vsa/tjoonerapp/MainActivity.java
+++ app/src/main/java/ehi2vsa/tjoonerapp/MainActivity.java
@@ -20,10 +20,10 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import ehi2vsa.tjoonerapp.async.LoginTask;
 import ehi2vsa.tjoonerapp.singletons.LoginToken;
+
 public class MainActivity extends AppCompatActivity {
-    URL url;
-    HttpURLConnection urlConnection;
     EditText usernameET, passwordET;
     LoginToken code = LoginToken.getInstance();
     Button button, loginhack;
@@ -84,61 +84,5 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-    }
-
-    private class LoginTask extends AsyncTask<String, String, String> {
-        String username;
-        String password;
-
-        public LoginTask(String username, String password) {
-            this.username = username;
-            this.password = password;
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            try {
-                url = new URL("http://setup.tjooner.tv/JCC/Saxion/TJOONER/REST/api/login");
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setConnectTimeout(10000);
-                urlConnection.setDoOutput(true);
-                urlConnection.setRequestMethod("POST");
-
-
-                urlConnection.setRequestProperty("Content-type", "application/json");
-                urlConnection.setRequestProperty("Accept", "application/json");
-                String req = "{\n \"Username\": \"" + username + "\", \n \"Password\": \"" + password + "\"\n";
-                urlConnection.setRequestProperty("Content-length", req.getBytes().length + "");
-                urlConnection.setDoInput(true);
-                urlConnection.setUseCaches(false);
-                urlConnection.connect();
-
-                OutputStream out = urlConnection.getOutputStream();
-                out.write(req.getBytes("UTF-8"));
-                out.flush();
-                out.close();
-
-                int statusCode = urlConnection.getResponseCode();
-
-                Log.d("failed", "doInBackground(): connection failed: statusCode: " + statusCode);
-                String response = urlConnection.getResponseMessage();
-                Log.d("ResponseConnection", response);
-
-                InputStream in = new BufferedInputStream(
-                        urlConnection.getInputStream());
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                StringBuilder result = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                }
-                System.out.println(result.toString());
-                return result.toString();
-            } catch (IOException a) {
-                System.out.println(a.getMessage());
-            }
-            return "Error";
-        }
     }
 }
