a78797f94c6184949da51bdfdff89478a1e658b9
stan <stansmeitink@hotmail.com>
2016-10-18 16:09:57 +0200

    kan fotos maken en opslaan lokaal. de intent waar ik de thumbnail uit wil ophalen is alleen ineens leeg

--- app/src/main/java/ehi2vsa/tjoonerapp/LoggedIn.java
+++ app/src/main/java/ehi2vsa/tjoonerapp/LoggedIn.java
@@ -1,18 +1,11 @@
 package ehi2vsa.tjoonerapp;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -21,11 +14,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import ehi2vsa.tjoonerapp.async.GetAllLocalImagesAsync;
 import ehi2vsa.tjoonerapp.async.GlobalAsync;
@@ -36,8 +24,7 @@ import ehi2vsa.tjoonerapp.fragments.SettingFragment;
 import ehi2vsa.tjoonerapp.fragments.VideoFragment;
 import ehi2vsa.tjoonerapp.singletons.LoginToken;
 
-public class LoggedIn extends FragmentActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class LoggedIn extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener {
     CameraFragment camera;
     MediaFragment media;
     VideoFragment video;
@@ -53,10 +40,6 @@ public class LoggedIn extends FragmentActivity
     String ACCESS_TOKEN = "ACCESS_TOKEN";
     String USER = "USER";
     final String PREFS_NAME = "MyPrefsFile";
-
-    //test Stan
-    String mCurrentPhotoPath;
-    static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,7 +130,6 @@ public class LoggedIn extends FragmentActivity
                     break;
                 case R.id.nav_camera:
                     toolbar.setTitle(R.string.camera);
-                    dispatchTakePictureIntent();
                     camera.setArguments(getIntent().getExtras());
                     getFragmentManager().beginTransaction().replace(R.id.fragment_container, camera).commit();
                     break;
@@ -179,46 +161,5 @@ public class LoggedIn extends FragmentActivity
         drawer.closeDrawer(GravityCompat.START);
 
         return true;
-    }
-
-    //test Stan
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
-
-    //test Stan
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                camera.startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
     }
 }
--- app/src/main/java/ehi2vsa/tjoonerapp/fragments/CameraFragment.java
+++ app/src/main/java/ehi2vsa/tjoonerapp/fragments/CameraFragment.java
@@ -10,7 +10,6 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,10 +24,11 @@ import java.util.Date;
 import ehi2vsa.tjoonerapp.R;
 
 public class CameraFragment extends Fragment {
-    private static final String TAG = CameraFragment.class.getSimpleName();
-    public static int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
-    ImageView ivResult;
+    private ImageView ivResult;
+    private String mCurrentPhotoPath;
 
     @Nullable
     @Override
@@ -36,26 +36,78 @@ public class CameraFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
         ivResult = (ImageView) view.findViewById(R.id.ivResult);
-
+        Button btnCamera = (Button) view.findViewById(R.id.btnCamera);
         Button btnSave = (Button) view.findViewById(R.id.btnSave);
-        btnSave.setOnClickListener( new View.OnClickListener() {
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //save
 
             }
         });
+
+        dispatchTakePictureIntent();
 
         return view;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ivResult.setImageBitmap(imageBitmap);
-        } else {
-            Log.i(TAG, "requestCode: " + requestCode + "resultCode: " + resultCode);
+
+            System.out.println("requestCode: " + requestCode + " resultCode: " + resultCode + " Activity.RESULT_OK: " + Activity.RESULT_OK + " data: " + data);
+
+//          Bitmap imageBitmap = (Bitmap) extras.get("data");
+//          ivResult.setImageBitmap(imageBitmap);
         }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getActivity(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 }
